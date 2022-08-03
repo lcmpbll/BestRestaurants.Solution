f86bc1e7dfda34f9c0d7a8ec425dbb0bc965d8b2
Liam Cammpbell <liam22campbell@gmail.com>
2022-08-03 09:53:46 -0700

    Set up for many to many database relationship
    Co-authored-by: Zachary Waggoner <zacwaggoner42@gmail.com>

--- BestRestaurants/Models/Restaurant.cs
+++ BestRestaurants/Models/Restaurant.cs
@@ -1,11 +1,18 @@
+using System.Collections.Generic;
+
 namespace BestRestaurants.Models
 {
   public class Restaurant
   {
+    public Restaurant()
+    {
+      this.JoinEntities = new HashSet<CuisineRestaurant>();
+    }
+    
     public int RestaurantId { get; set; }
     public string Name { get; set; }
     public string Description { get; set; }
-    public int CuisineId { get; set; }
-    public virtual Cuisine Cuisine { get; set; }
+    
+    public virtual ICollection<CuisineRestaurant> JoinEntities { get; }
   }
 }
