0aa440bffbd8f4686c11224a750a8850892ac4dc
Liam Cammpbell <liam22campbell@gmail.com>
2022-08-03 09:54:20 -0700

    Create migrations
    Co-authored-by: Zachary Waggoner <zacwaggoner42@gmail.com>

---
+++ BestRestaurants/Migrations/20220803165029_Initial.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BestRestaurants.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Cuisine",
+                columns: table => new
+                {
+                    CuisineId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(type: "longtext CHARACTER SET utf8mb4", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cuisine", x => x.CuisineId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Restaurant",
+                columns: table => new
+                {
+                    RestaurantId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(type: "longtext CHARACTER SET utf8mb4", nullable: true),
+                    Description = table.Column<string>(type: "longtext CHARACTER SET utf8mb4", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Restaurant", x => x.RestaurantId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CuisineRestaurant",
+                columns: table => new
+                {
+                    CuisineRestaurantId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    RestaurantId = table.Column<int>(type: "int", nullable: false),
+                    CuisineId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CuisineRestaurant", x => x.CuisineRestaurantId);
+                    table.ForeignKey(
+                        name: "FK_CuisineRestaurant_Cuisine_CuisineId",
+                        column: x => x.CuisineId,
+                        principalTable: "Cuisine",
+                        principalColumn: "CuisineId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_CuisineRestaurant_Restaurant_RestaurantId",
+                        column: x => x.RestaurantId,
+                        principalTable: "Restaurant",
+                        principalColumn: "RestaurantId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CuisineRestaurant_CuisineId",
+                table: "CuisineRestaurant",
+                column: "CuisineId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CuisineRestaurant_RestaurantId",
+                table: "CuisineRestaurant",
+                column: "RestaurantId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CuisineRestaurant");
+
+            migrationBuilder.DropTable(
+                name: "Cuisine");
+
+            migrationBuilder.DropTable(
+                name: "Restaurant");
+        }
+    }
+}
