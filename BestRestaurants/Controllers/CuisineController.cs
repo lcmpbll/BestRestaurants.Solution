6c9777e521deb87da85bd5da95f0c3c8a4c42ab8
Liam Cammpbell <liam22campbell@gmail.com>
2022-08-03 10:51:45 -0700

    Update views for many to many relationships.
    Co-authored-by: Zachary Waggoner <zacwaggoner42@gmail.com>

--- BestRestaurants/Controllers/CuisineController.cs
+++ BestRestaurants/Controllers/CuisineController.cs
@@ -15,57 +15,57 @@ namespace BestRestaurants.Controllers
       _db = db;
     }
 
-    public ActionResult Index()
-    {
-      List<Cuisine> model = _db.Cuisine.ToList();
-      return View(model);
-    }
+//     public ActionResult Index()
+//     {
+//       List<Cuisine> model = _db.Cuisine.ToList();
+//       return View(model);
+//     }
 
-    public ActionResult Create()
-    {
-      return View();
-    }
+//     public ActionResult Create()
+//     {
+//       return View();
+//     }
 
-    [HttpPost]
-    public ActionResult Create(Cuisine cuisine)
-    {
-      _db.Cuisine.Add(cuisine);
-      _db.SaveChanges();
-      return RedirectToAction("Index");
-    }
+//     [HttpPost]
+//     public ActionResult Create(Cuisine cuisine)
+//     {
+//       _db.Cuisine.Add(cuisine);
+//       _db.SaveChanges();
+//       return RedirectToAction("Index");
+//     }
 
-    public ActionResult Details(int id)
-    {
-      Cuisine thisCuisine = _db.Cuisine.FirstOrDefault(cuisine => cuisine.CuisineId == id);
-      return View(thisCuisine);
-    }
-    public ActionResult Edit(int id)
-    {
-      var thisCuisine = _db.Cuisine.FirstOrDefault(cuisine => cuisine.CuisineId == id);
-      return View(thisCuisine);
-    }
+//     public ActionResult Details(int id)
+//     {
+//       Cuisine thisCuisine = _db.Cuisine.FirstOrDefault(cuisine => cuisine.CuisineId == id);
+//       return View(thisCuisine);
+//     }
+//     public ActionResult Edit(int id)
+//     {
+//       var thisCuisine = _db.Cuisine.FirstOrDefault(cuisine => cuisine.CuisineId == id);
+//       return View(thisCuisine);
+//     }
 
-    [HttpPost]
-    public ActionResult Edit(Cuisine cuisine)
-    {
-      _db.Entry(cuisine).State = EntityState.Modified;
-      _db.SaveChanges();
-      return RedirectToAction("Index");
-    }
+//     [HttpPost]
+//     public ActionResult Edit(Cuisine cuisine)
+//     {
+//       _db.Entry(cuisine).State = EntityState.Modified;
+//       _db.SaveChanges();
+//       return RedirectToAction("Index");
+//     }
 
-    public ActionResult Delete(int id)
-    {
-      var thisCuisine = _db.Cuisine.FirstOrDefault(cuisine => cuisine.CuisineId == id);
-      return View(thisCuisine);
-    }
+//     public ActionResult Delete(int id)
+//     {
+//       var thisCuisine = _db.Cuisine.FirstOrDefault(cuisine => cuisine.CuisineId == id);
+//       return View(thisCuisine);
+//     }
 
-    [HttpPost, ActionName("Delete")]
-    public ActionResult DeleteConfirmed(int id)
-    {
-      var thisCuisine = _db.Cuisine.FirstOrDefault(cuisine => cuisine.CuisineId == id);
-      _db.Cuisine.Remove(thisCuisine);
-      _db.SaveChanges();
-      return RedirectToAction("Index");
-    }
+//     [HttpPost, ActionName("Delete")]
+//     public ActionResult DeleteConfirmed(int id)
+//     {
+//       var thisCuisine = _db.Cuisine.FirstOrDefault(cuisine => cuisine.CuisineId == id);
+//       _db.Cuisine.Remove(thisCuisine);
+//       _db.SaveChanges();
+//       return RedirectToAction("Index");
+//     }
   }
 }
